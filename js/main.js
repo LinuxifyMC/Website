825a26a4061d17a3850e3656c92249f501a08ff3
opuadm <opuadm@gmail.com>
2025-10-28 21:01:28 +0000

    Added favicon + Fixed uncaught TypeError

--- js/main.js
+++ js/main.js
@@ -1,17 +1,30 @@
-const maintxt_topbar = document.getElementById("maintxt_topbar");
-const about_topbar = document.getElementById("about_topbar");
-const github_logo = document.getElementById("github-logo");
-const twitter_logo = document.getElementById("twitter-logo");
+document.addEventListener('DOMContentLoaded', () => {
+    const maintxt_topbar = document.getElementById("maintxt_topbar");
+    const about_topbar = document.getElementById("about_topbar");
+    const github_logo = document.getElementById("github-logo");
+    const twitter_logo = document.getElementById("twitter-logo");
 
-maintxt_topbar.addEventListener("click", () => {
-    window.location.href = "/";
-});
-about_topbar.addEventListener("click", () => {
-    window.location.href = "/about.php";
-});
-github_logo.addEventListener("click", () => {
-    window.location.href = "https://github.com/LinuxifyMC";
-});
-twitter_logo.addEventListener("click", () => {
-    window.location.href = "https://x.com/LinuxifyMC";
+    if (maintxt_topbar) {
+        maintxt_topbar.addEventListener("click", () => {
+            window.location.href = "/";
+        });
+    }
+
+    if (about_topbar) {
+        about_topbar.addEventListener("click", () => {
+            window.location.href = "/about.php";
+        });
+    }
+
+    if (github_logo) {
+        github_logo.addEventListener("click", () => {
+            window.location.href = "https://github.com/LinuxifyMC";
+        });
+    }
+
+    if (twitter_logo) {
+        twitter_logo.addEventListener("click", () => {
+            window.location.href = "https://x.com/LinuxifyMC";
+        });
+    }
 });
